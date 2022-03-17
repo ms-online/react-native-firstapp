52cc51b1b6e0aea24cdee03e1a8cfced265d757e
ms-online <27732357@qq.com>
2022-03-17 20:44:58 +0800

    lesson-12

--- App.js
+++ App.js
@@ -7,55 +7,61 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, ScrollView, RefreshControl} from 'react-native';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+    {key: 9, item: 'Item 9'},
+  ]);
+  const [Refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...items, {key: 14, item: 'Item 11'}]);
+    setRefreshing(false);
+  };
   return (
-    <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}> 1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}> 2</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}> 3</Text>
-      </View>
-    </View>
+    <ScrollView
+      style={styles.body}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+          colors={['#ff00ff']}
+        />
+      }>
+      {items.map(obj => {
+        return (
+          <View style={styles.item} key={obj.key}>
+            <Text style={styles.text}>{obj.item}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#0000ff',
-    //决定子元素沿着次轴的排列方式
-    alignItems: 'center',
-    //决定子元素沿着主轴的排列方式
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
   },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
+  item: {
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-  },
-  view2: {
-    flex: 1,
-    backgroundColor: '#ff00ff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 1,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
   },
   text: {
-    color: '#ffffff',
+    color: '#000000',
     fontSize: 30,
     margin: 10,
   },
